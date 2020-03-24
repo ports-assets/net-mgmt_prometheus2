9c0b4144dc3f0bd974b5239633a2a5f701696753
David O'Rourke <dor.bsd@xm0.uk>
2020-03-24 21:00:12 +0000

    Add makeasset.sh script
    This helps to quickly compile the asset for a new Prometheus version

---
+++ makeasset.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# Path to the asset file
+asset_file="web/ui/assets_vfsdata.go"
+
+# Upstream Prometheus repo
+prometheus_repo="https://github.com/prometheus/prometheus.git"
+
+# Shallow clones a single repository branch
+clone() {
+    local repo="$1"
+    local branch="$2"
+    local output_dir="$3"
+
+    git clone \
+        --depth=1 \
+        --branch="${branch}" \
+        --single-branch \
+        "${repo}" \
+        "${output_dir}"
+}
+
+# Heads into $dir and executes `gmake assets`
+make_asset() {
+    local dir="$1"
+
+    ( \
+        cd "${dir}" \
+        && gmake assets \
+    )
+}
+
+# Copy the asset from the clone dir to its correct location
+copy_asset() {
+    local dir="$1"
+
+    cp -v \
+        "${dir}/${asset_file}" \
+        "${asset_file}"
+}
+
+# Entry point
+main() {
+    local version="$1"
+    local clone_dir="prometheus"
+
+    if [ -z "${version}" ]; then
+        echo "Must provide a Prometheus version (tag or branch) to clone" >&2
+        echo "Usage: $0 <VERSION>" >&2
+        exit 1
+    fi
+
+    clone "${prometheus_repo}" "${version}" "${clone_dir}"
+    make_asset "${clone_dir}"
+    copy_asset "${clone_dir}"
+}
+
+main "$@"
